910d14688adc15dd22a46852e4f92294efd2a748
Mitchell Bird <mitchell.bird@live.co.uk>
2026-01-02 02:39:57 +0000

    Initialise angular typescript project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,css}", // include Angular templates and TypeScript files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
